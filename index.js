e54cd8638ef2389a02d707ffee7c88369c341688
BenThere <benjaminbirdsall@icloud.com>
2024-04-07 11:21:47 -0600

    fix recently opened logic

--- index.js
+++ index.js
@@ -21,11 +21,10 @@ async function scrapeTempleDetails(url, existingData, temple) {
         await browser.close();
 
         // Format the address
-        // Format the address
         if (address) {
             // Find the corresponding temple in existing data
             const existingTempleIndex = existingData.findIndex(item => item.Name === temple.Name);
-            if (existingTempleIndex !== -1) {
+            if (existingTempleIndex !== -1 && !existingData[existingTempleIndex].Address) { // Check if the address is not already updated
                 console.log('Updating address for:', temple.Name);
                 existingData[existingTempleIndex].Address = address.trim().replace(/\n/g, ', '); // Replace newline characters with ', '
             }
@@ -82,21 +81,39 @@ async function scrapeTempleList(url) {
         for (const temple of templeData) {
             if (temple.Location && temple.Location.toLowerCase().includes('utah')) {
                 utahTempleCount++; // Increment the counter for Utah temples
-
-                // If the temple previously had 'announced', 'renovation', or 'construction', but now has a date and address, it's opened
-                if ((temple.Date.toLowerCase() === 'announced' || temple.Date.toLowerCase() === 'renovation' || temple.Date.toLowerCase() === 'construction') && temple.Address) {
-                    recentlyOpenedTemples.push(temple.Name);
-                }
-
+        
                 // Find the corresponding temple in existing data
                 const existingTempleIndex = existingData.findIndex(item => item.Name === temple.Name);
+        
+                if (existingTempleIndex !== -1) {
+                    // If the temple previously had 'announced', 'renovation', or 'construction', and now has a valid date and address, it's opened
+                    if ((existingData[existingTempleIndex].Date.toLowerCase() === 'announced' || existingData[existingTempleIndex].Date.toLowerCase() === 'renovation' || existingData[existingTempleIndex].Date.toLowerCase() === 'construction') &&
+                        (temple.Date && temple.Date.toLowerCase() !== 'announced' && temple.Date.toLowerCase() !== 'renovation' && temple.Date.toLowerCase() !== 'construction') && temple.Address) {
+                            recentlyOpenedTemples.push(temple.Name);
+                    } else {
+                        // console.log(temple.Name, existingData[existingTempleIndex].Date.toLowerCase(), temple.Date)
+                    }
 
-                // Update the temple data with details if it doesn't have an address in existing data or if the newly scraped data has a valid address
-                if (existingTempleIndex !== -1 && (!existingData[existingTempleIndex].Address || temple.Address)) {
-                    console.log('Updating address for:', temple.Name);
-                    existingData[existingTempleIndex].Address = temple.Address;
+                    const existingDate = existingData[existingTempleIndex].Date.toLowerCase().trim()
+                    const newDate = temple.Date.toLowerCase().trim()
+                    if (existingDate != newDate) {
+                        if (existingDate == 'renovation' || existingDate == 'construction') {
+                            recentlyOpenedTemples.push(temple.Name);
+                        }
+                    }
+                
+                    // Update the temple data with details if it doesn't have an address in existing data or if the newly scraped data has a valid address
+                    if (!existingData[existingTempleIndex].Address || temple.Address) {
+                        // console.log('Updating address for:', temple.Name);
+                        existingData[existingTempleIndex].Address = temple.Address;
+                    }
+                
+                    // Update the temple date if templeData has a valid date
+                    if (temple.Date && temple.Date.toLowerCase() !== 'announced' && temple.Date.toLowerCase() !== 'renovation' && temple.Date.toLowerCase() !== 'construction') {
+                        existingData[existingTempleIndex].Date = temple.Date;
+                    }
                 }
-
+                
                 // Call scrapeTempleDetails if the temple doesn't have an address
                 if (!existingData[existingTempleIndex].Address) {
                     console.log('Searching temple details for:', temple.Name);
@@ -107,10 +124,10 @@ async function scrapeTempleList(url) {
             } else {
                 // console.log('Skipping temple:', temple.Name);
             }
-        }
+        }        
 
         console.log('Number of Utah temples:', utahTempleCount); // Log the number of Utah temples
-        console.log('Temples recently opened:', recentlyOpenedTemples.join(', ')); // Log recently dedicated temples
+        console.log('Temples recently opened:', recentlyOpenedTemples.join(', ')); // Log recently opened temples
 
         // Write updated data back to temples.json
         fs.writeFileSync('temples.json', JSON.stringify(existingData, null, 2));
